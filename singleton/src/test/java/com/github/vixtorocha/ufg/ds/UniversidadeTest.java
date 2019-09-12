662954e5a22c773985f378eb7b0b397539758fbe
kyriosdata <fabio@inf.ufg.br>
2019-09-12 09:22:19 -0300

    testes expõem problema com implementacao

--- singleton/src/test/java/com/github/vixtorocha/ufg/ds/UniversidadeTest.java
+++ singleton/src/test/java/com/github/vixtorocha/ufg/ds/UniversidadeTest.java
@@ -1,15 +1,32 @@
 package com.github.vixtorocha.ufg.ds;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
 
 class UniversidadeTest {
+
+    UniversidadeTest() {
+        fazTesteFalharDesmonstraQuebraDeEncapsulamento();
+    }
+
+    void fazTesteFalharDesmonstraQuebraDeEncapsulamento() {
+        Universidade.setInstituicoes(null);
+    }
 
     @Test
     void recuperaInstancia() {
         final Universidade universidade = Universidade.getInstance();
         final String[] instituicoes = universidade.getInstituicoes();
         assertEquals("INF", instituicoes[0]);
+    }
+
+    @Test
+    void instanciasRecuperadasSaoIguais() {
+        final Universidade u1 = Universidade.getInstance();
+        final Universidade u2 = new Universidade();
+
+        assertSame(u1, u2, "não podem existir duas instâncias");
     }
 }
