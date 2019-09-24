7ada457d40cf65bc9af309aa78e129caa0208c9b
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-24 09:18:44 -0300

    Teste sujeito a falha

--- classe/src/test/java/com/github/vixtorocha/ufg/ds/ClasseCriadaTest.java
+++ classe/src/test/java/com/github/vixtorocha/ufg/ds/ClasseCriadaTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class ClasseCriadaTest {
+    
+    // TODO conforme está seu teste é, em inglês, "brittle" ou "frágil". Veja um bom exemplo de como fazer isso https://github.com/newtonjose/ds-2019-02/blob/master/auto-closeable/src/test/java/com/github/newtonjose/autocloseable/FileInputStreamAutoCloseableTest.java
 
     @Test
     void verificaCloseChamado() {
